c360c109a392eac0d95be6b96248b9909e6b12ee
selocuba <124109654+selocuba@users.noreply.github.com>
2025-01-29 07:33:16 +0100

    Update jtag_devs.c

--- src/target/jtag_devs.c
+++ src/target/jtag_devs.c
@@ -87,7 +87,7 @@ const jtag_dev_descr_s dev_descr[] = {
 		.descr = "STM32F3xx.",
 	},
 	{
-		.idcode = 0x2d43b041,
+		.idcode = 0x2d43b041U,
 		.idmask = 0xffffffffU,
 		.descr = "STM32F4xx.",
 	},
